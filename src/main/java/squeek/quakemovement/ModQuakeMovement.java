63924a12bc08e741bd28baed251e90f75d702747
PurplePrint <sharethekitten909@gmail.com>
2025-04-09 01:24:43 +0300

    Add files via upload

---
+++ src/main/java/squeek/quakemovement/ModQuakeMovement.java
@@ -0,0 +1,63 @@
+package squeek.quakemovement;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+//@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION, name="Squake", acceptedMinecraftVersions="[1.12.2]", dependencies = "after:squeedometer", guiFactory = ModInfo.CONFIG_GUI_FACTORY_CLASS)
+@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION, name="Squake", acceptedMinecraftVersions="[1.12.2]", dependencies = "after:squeedometer;after:techguns;after:openglider", guiFactory = ModInfo.CONFIG_GUI_FACTORY_CLASS)
+public class ModQuakeMovement
+{
+
+	public static boolean techgunsIntegration = false;
+	public static boolean openGliderIntegration = false;
+
+	// The instance of your mod that Forge uses.
+	@Instance(value = ModInfo.MODID)
+	public static ModQuakeMovement instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
+		techgunsIntegration = Loader.isModLoaded("techguns");
+		openGliderIntegration = Loader.isModLoaded("openglider");
+
+		ModConfig.init(event.getSuggestedConfigurationFile());
+		MinecraftForge.EVENT_BUS.register(new ModConfig());
+		MinecraftForge.EVENT_BUS.register(this);
+		if (event.getSide() == Side.CLIENT)
+		{
+			MinecraftForge.EVENT_BUS.register(new ToggleKeyHandler());
+		}
+
+//		if (event.getSide() == Side.SERVER) {
+//			MinecraftForge.EVENT_BUS.register(new FairplayPatch());
+//		}
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+	}
+
+	@SubscribeEvent
+	public void onLivingFall(LivingFallEvent event)
+	{
+		if (!(event.getEntityLiving() instanceof EntityPlayer))
+			return;
+
+		if (ModConfig.INCREASED_FALL_DISTANCE != 0.0D)
+		{
+			event.setDistance((float) (event.getDistance() - ModConfig.INCREASED_FALL_DISTANCE));
+		}
+	}
+}
